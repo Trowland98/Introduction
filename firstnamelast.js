d387ce73be3e1cd5db9ff9ed3abe484565cfc665
Trowland98 <70236298+Trowland98@users.noreply.github.com>
2020-08-31 17:29:43 -0600

    ObjectOriented
    Set user as a class but was not able to retrieve the user's inputted name. "Class constructor User cannot be invoked without 'new'."

--- firstnamelast.js
+++ firstnamelast.js
@@ -1,7 +1,12 @@
-function User() {
-    var firstName=document.getElementById('firstName').value;
-    var lastName=document.getElementById('lastName').value;
-    var FullName=firstName + " " +lastName;
-    document.getElementById("result").innerHTML="Your full name is " + FullName;
-    console.log(FullName)
+class User {
+    constructor(first, last) {
+      this.first = document.getElementById('firstName').value;
+      this.last = document.getElementById('lastName').value;
     }
+  present() {
+    return "Your full name is " + this.first + " " + this.last
+  }
+}
+
+let fullName = new User(firstName, lastName);
+document.getElementById("result").innerHTML = fullName.present();
