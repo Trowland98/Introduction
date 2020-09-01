38ab4809c50b112ecaddc98604cf872db4c7d3c6
Trowland98 <70236298+Trowland98@users.noreply.github.com>
2020-09-01 17:10:39 -0600

    Update firstnamelast.js

--- firstnamelast.js
+++ firstnamelast.js
@@ -1,23 +1,7 @@
-class User {
-  constructor(first) {
-    first = document.getElementById("firstName").value;
-    this.firstname = first;
-  }
-  present() {
-    return 'Your name must be ' + this.firstname + " ";
-  }
-}
-
-class Sur extends User {
-  constructor(first, last) {
-    super(first);
-    last = document.getElementById("lastName").value;
-    this.lastname = last
-  }
-  show() {
-    return this.present() + this.lastname;
-  }
-}
-
-fullname = new Sur(this.firstname, this.lastname);
-document.getElementById("result").innerHTML = fullname.show();
+function User() {
+    var firstName=document.getElementById('firstName').value;
+    var lastName=document.getElementById('lastName').value;
+    var FullName=firstName + " " +lastName;
+    document.getElementById("result").innerHTML="Your full name is " + FullName;
+    console.log(FullName)
+    }
